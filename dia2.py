702b24fa04921ddf49b18a1df7579b4e959b5723
andrew@2cb <andres.fmca28@gmail.com>
2026-08-31 21:02:40 -0600

    Dia 2: variables, tipos de datos, calculadora de propinas

---
+++ dia2.py
@@ -0,0 +1,16 @@
+nombre = "Andres"
+edad = 29
+altura = 1.69
+es_ingeniero = True
+
+print(nombre)
+print(edad)
+print(altura)
+print(es_ingeniero)
+
+print(type(nombre))
+print(type(edad))
+
+
+
+
---
+++ propina.py
@@ -0,0 +1,8 @@
+cuenta = 25.50
+porcentaje = 0.15
+propina = cuenta * porcentaje
+total = cuenta + porcentaje
+
+print("Cuenta", cuenta)
+print("Propina", propina)
+print("Total", total)
