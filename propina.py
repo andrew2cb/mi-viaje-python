6c92370eb884aed3a5b38919821ab25e175b545f
andrew@2cb <andres.fmca28@gmail.com>
2026-09-01 18:04:13 -0600

    Corrige bug en calculo de total

--- propina.py
+++ propina.py
@@ -1,7 +1,7 @@
 cuenta = 25.50
 porcentaje = 0.15
-propina = cuenta * porcentaje
-total = cuenta + porcentaje
+propina = round(cuenta * porcentaje, 2)
+total = round(cuenta + propina, 2)
 
 print("Cuenta", cuenta)
 print("Propina", propina)
