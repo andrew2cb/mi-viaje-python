fef06ec65fd019f6298fc6f6fe32258279e479e7
andrew2cb <andres.fmca28@gmail.com>
2026-09-10 07:57:14 -0600

    Semana 2 Dia 2: tabla de multiplicar y FizzBuzz

---
+++ dia8.py
@@ -0,0 +1,24 @@
+#frutas = ["manzana", "plátano", "naranja"]
+
+#for fruta in frutas:
+    #print(f"Me gusta la {fruta}")
+
+#elige_un_numero = int(input("Elige un número: "))
+#for i in range(1, 11):
+    #print(f"{elige_un_numero} * {i} = {i * elige_un15
+    #_numero}")
+
+elige_un_numero = int(input("Elige un número: "))
+for i in range(1, 21):
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
+    
+
+
+    
